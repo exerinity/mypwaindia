06b08ee284442aafa744fd0132f3f2c7f69645c3
exerinity <me@exerinity.com>
2026-05-18 17:58:01 +1000

    m

--- src/main.tsx
+++ src/main.tsx
@@ -29,7 +29,7 @@ function Root() {
   );
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('r')!).render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>
