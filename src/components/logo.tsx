b71975ed194eccaaadae59e2cdb6e71c6beda854
exerinity <me@exerinity.com>
2026-05-20 13:30:40 +1000

    m

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,4 +1,4 @@
-export function Logo({ height = 38, className = 'app-header-logo' }: { height?: number; className?: string }) {
+export function Logo({ height = 38, className = 'mpi-header-logo' }: { height?: number; className?: string }) {
   return (
     <img
       src="/i/logo-full.png"
