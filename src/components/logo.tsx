920c67600cc543d3e54201b770045979d4029953
exerinity <me@exerinity.com>
2026-05-20 12:22:10 +1000

    m

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,7 +1,7 @@
 export function Logo({ height = 38, className = 'app-header-logo' }: { height?: number; className?: string }) {
   return (
     <img
-      src="https://mypayindia.com/siteassets/images/branding/logo-full.png"
+      src="/i/logo-full.png"
       alt="MyPayIndia"
       height={height}
       className={className}
