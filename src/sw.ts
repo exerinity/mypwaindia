e9bd130d6fd082e41c61d28a4c51a43640a4062c
exerinity <me@exerinity.com>
2026-06-07 21:39:10 +1000

    m

--- src/sw.ts
+++ src/sw.ts
@@ -7,7 +7,9 @@ import { clientsClaim } from 'workbox-core';
 
 declare let self: ServiceWorkerGlobalScope;
 
-self.skipWaiting();
+self.addEventListener('message', (event) => {
+  if (event.data?.type === 'SKIP_WAITING') self.skipWaiting();
+});
 clientsClaim();
 
 cleanupOutdatedCaches();
