19b3555c1bdcedb9142dfc900bd192acd655814d
exerinity <me@exerinity.com>
2026-08-02 14:19:10 +1000

    No

--- vite.config.js
+++ vite.config.js
@@ -78,7 +78,7 @@ export default defineConfig({
   },
   build: {
     modulePreload: false,
-    minify: 'esbuild',
+    minify: false,
     cssMinify: false,
     sourcemap: false,
     target: 'esnext',
