9d355e77a5e49fcc8943db8fb552f751dc70fc72
exerinity <me@exerinity.com>
2026-08-21 14:51:00 +1000

    m

--- src/pages/information/release_notes_notes.tsx
+++ src/pages/information/release_notes_notes.tsx
@@ -11,6 +11,13 @@ export type Release = {
 
 export const RELEASES: Release[] = [
   {
+    version: '23',
+    date: '21 Aug 2026',
+    notes: [
+      <>Added <Link to="/i/agent">MyAgentIndia</Link></>
+    ]
+  },
+  {
     version: '22a',
     date: '9 Aug 2026',
     notes: [
